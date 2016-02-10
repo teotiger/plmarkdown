e656ea53ec48a846374c9de8cfc87ce81902dc8e
teotiger <markusschulze1704@gmx.de>
2016-02-10 22:35:20 +0100

    improve install script

--- setup/install.sql
+++ setup/install.sql
@@ -1,13 +1,10 @@
 set scan off;
-prompt **********************************
-prompt ** Install Script for PLMarkdown**
-prompt **********************************
+prompt ***********************************
+prompt ** Install Script for PLMarkdown **
+prompt ***********************************
 prompt
 
 prompt 1) Install type specifications...
-@@plmarkdown.pks
+@@plmarkdown.pls
 prompt 2) Install package bodies...
-@@plmarkdown.pkb
-
-prompt ==> All types successfully installed!
-prompt
+@@plmarkdown.plb
